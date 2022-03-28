604da2b82ab6adf7ea9418ecd93b233ceaf34e97
ldulivo <ldulivo@gmail.com>
2022-03-28 17:35:37 -0300

    test-01 2022.03.28

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AboutMe from './pages/AboutMe';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
-      
-    hola
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home /> } />
+          <Route path='/aboutme' element={<AboutMe />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
