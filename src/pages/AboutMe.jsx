a7b0407f7c076892a067a3fab6f9606aa935b1aa
ldulivo <ldulivo@gmail.com>
2022-03-28 18:10:04 -0300

    test-02 2022.03.28

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <div>
+        <h1>AboutMe</h1>
+        <Link to={'/'}>
+            <p>Back to Home</p>
+        </Link>
+    </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Home</h1>
+        <Link to={'/aboutme'} >
+            <p>AboutMe</p>
+        </Link>
+    </div>
   )
 }
